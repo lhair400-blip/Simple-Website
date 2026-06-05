88e6c7192476c289ff11303a11b479616baa912c
lhair400-blip <lhair400@gmail.com>
2026-06-05 12:59:50 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,158 @@
-'use strict';
+function saveData(key,data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
 
-const switcher = document.querySelector('.btn');
+function getData(key) {
+    return JSON.parse(localStorage.getItem(key)) || [];
+}
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+function addTask() {
+    const input = document.getElementById("taskInput");
 
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark mode";
-    } else {
-        this.textContent = "Light mode";
+    if (!input || input.value.trim() === "") return;
+
+    const tasks = getData("tasks");
+
+    tasks.push({
+        text:input.value,
+        completed: false
+    });
+
+    saveData("tasks", tasks);
+    input.value = "";
+    loadTasks();
+}
+
+function loadTasks() {
+    const list = document.getElementById("taskList");
+
+    if (!list) return;
+
+    list.innerHTML = "";
+    const tasks = getData("tasks");
+
+    tasks.forEach((task, index) => {
+        const div = document.createElement("div");
+        div.className = "list-item";
+
+        div.innerHTML =`
+            <span class = "${task.completed ? 'completed' : ''}">
+                ${task.text}
+            </span>
+
+            <div>
+                <button onclick="toggleTask(${index})">✓</button>
+                <button class="delete-btn" onclick="deleteTask(${index})">X</button>
+            </div>
+        `;
+
+        list.appendChild(div);
+    });
+}
+
+function toggleTask(index) {
+    const tasks = getData("tasks");
+    tasks[index].completed = !tasks[index].completed;
+    saveData("tasks", tasks);
+    loadTasks();
+}
+
+function deleteTask(index) {
+    const tasks = getData("tasks");
+    tasks.splice(index, 1);
+    saveData("tasks", tasks);
+    loadTasks();
+}
+
+function addNote(){
+    const input = document.getElementById("noteInput");
+
+    if (!input || input.value.trim() === "") return;
+
+    const notes = getData("notes");
+
+    notes.push(input.value);
+    saveData("notes", notes);
+    input.value = "";
+    loadNotes();
+}
+
+function loadNotes() {
+    const list = document.getElementById("noteList");
+
+    if (!list) return;
+
+    list.innerHTML = "";
+    const notes = getData("notes");
+
+    notes.forEach((note, index) => {
+        const div = document.createElement("div");
+        div.className = "list-item";
+
+        div.innerHTML =`
+            <span>${note}</span>
+            <button class = "delete-btn"
+            onclick="deleteNote(${index})">Delete</button>
+        `;
+
+        list.appendChild(div);
+    });
+}
+
+function deleteNote(index) {
+    const notes = getData("notes");
+    notes.splice(index, 1);
+    saveData("notes", notes);
+    loadNotes();
+}
+
+function addGoal() {
+    const input = document.getElementById("goalInput");
+
+    if (!input || input.value.trim() === "") return;
+    const goals = getData("goals");
+
+    goals.push(input.value);
+    saveData("goals", goals);
+    input.value = "";
+    loadGoals();
+}
+
+function loadGoals() {
+    const list = document.getElementById("goalList");
+
+    if (!list) return;
+
+    list.innerHTML = "";
+    const goals = getData("goals");
+
+    goals.forEach((goal, index) => {
+        const div = document.createElement("div");
+        div.className = "list-item";
+
+        div.innerHTML =`
+            <span>${goal}</span>
+            <button class = "delete-btn"
+            onclick = "deleteGoal(${index})">Delete</button>
+        `;
+
+        list.appendChild(div);
+    });
+}
+
+function deleteGoal(index) {
+    const goals = getData("goals");
+    goals.splice(index, 1);
+    saveData("goals", goals);
+    loadGoals();
+}
+
+window.onload = () => {
+    try {
+        loadTasks();
+        loadNotes();
+        loadGoals();
+    } catch (error) {
+        console.error(error);
     }
-
-    console.log('current class name: ' + className);
-});
+};
